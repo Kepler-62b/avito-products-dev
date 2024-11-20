27b44d9b908166a54f2f6e21f66cbfa176ccaa73
adminap <shestopalov92@gmail.com>
2024-11-20 10:53:01 +0300

    dev: pre-release

---
+++ Messenger/UpdateAvitoProductStock/UpdateAvitoProductStockMessage.php
@@ -0,0 +1,112 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Avito\Products\Messenger\UpdateAvitoProductStock;
+
+use BaksDev\Products\Product\Type\Id\ProductUid;
+use BaksDev\Products\Product\Type\Offers\ConstId\ProductOfferConst;
+use BaksDev\Products\Product\Type\Offers\Variation\ConstId\ProductVariationConst;
+use BaksDev\Products\Product\Type\Offers\Variation\Modification\ConstId\ProductModificationConst;
+use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
+
+final readonly class UpdateAvitoProductStockMessage
+{
+    private UserProfileUid $profile;
+
+    private ProductUid $product;
+
+    private ProductOfferConst|null $offer;
+
+    private ProductVariationConst|null $variation;
+
+    private ProductModificationConst|null $modification;
+
+    public function __construct(
+        UserProfileUid|string $profile,
+        ProductUid|string $product,
+        ProductOfferConst|string|null $offer,
+        ProductVariationConst|string|null $variation,
+        ProductModificationConst|string|null $modification,
+    )
+    {
+        if(is_string($profile))
+        {
+            $profile = new UserProfileUid($profile);
+        }
+
+        if(is_string($product))
+        {
+            $product = new ProductUid($product);
+        }
+
+        if(is_string($offer))
+        {
+            $offer = new ProductOfferConst($offer);
+        }
+
+        if(is_string($variation))
+        {
+            $variation = new ProductVariationConst($variation);
+        }
+
+        if(is_string($modification))
+        {
+            $modification = new ProductModificationConst($modification);
+        }
+
+        $this->profile = $profile;
+
+        $this->product = $product;
+        $this->offer = $offer;
+        $this->variation = $variation;
+        $this->modification = $modification;
+    }
+
+    public function getProfile(): UserProfileUid
+    {
+        return $this->profile;
+    }
+
+    public function getProduct(): ProductUid
+    {
+        return $this->product;
+    }
+
+    public function getOfferConst(): ?ProductOfferConst
+    {
+        return $this->offer;
+    }
+
+    public function getVariationConst(): ?ProductVariationConst
+    {
+        return $this->variation;
+    }
+
+    public function getModificationConst(): ?ProductModificationConst
+    {
+        return $this->modification;
+    }
+}
